ef21405357bbeb7a94fb4f2f98e4893aa46962bc
vitor <vi.souza.almeida@protonmail.com>
2025-03-17 21:17:29 -0300

    refactor(header): get token from header

--- front/app/api/rooms/route.ts
+++ front/app/api/rooms/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-    // const token = req.headers.get('Authorization')?.split(' ')[1];
-
-    const token = localStorage.getItem('token');
+export async function GET(req: NextRequest) {
+    const token = req.headers.get('Authorization')?.split(' ')[1];
 
     const response = await fetch(`${process.env.BACKEND_ROOT_URL}/api/v1/rooms`, {
         method: 'GET',
